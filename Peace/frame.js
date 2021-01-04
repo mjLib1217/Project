29954c50f95144be2ed599be419bf6357f3d8642
mjLib1217 <mzecatjrotus1217@gmail.com>
2021-01-05 01:14:01 +0900

    Create Navigation Bar

--- Peace/frame.js
+++ Peace/frame.js
@@ -1,20 +1,29 @@
 import { Point } from './point.js'
+import { HeadPanel } from './headPanel.js'
 
 const START_X_POS   = 10;
 const START_Y_POS   = 10;
 const REDUCE_WIDTH  = 20;
 const REDUCE_HEIGHT = 20;
-const LINE_WIDTH    = 10;
+const LINE_WIDTH    = 5;
 const FRAME_LINE_COLOR = 'rgb(0, 0, 0)';
 
 export class Frame {
     constructor() {
         this.pos = new Point(START_X_POS, START_Y_POS); // Frame 이 그려지는 시작 좌표
+        
+        this.headPanel = new HeadPanel();
     }
 
     onDraw(context, stageWidth, stageHeight) {
         context.strokeStyle = FRAME_LINE_COLOR;
         context.lineWidth = LINE_WIDTH;
-        context.strokeRect(this.pos.x, this.pos.y, stageWidth - REDUCE_WIDTH, stageHeight - REDUCE_HEIGHT);    
+
+        let nFrameWidth = stageWidth - REDUCE_WIDTH;
+        let nFrameHeight = stageHeight - REDUCE_HEIGHT;
+
+        context.strokeRect(this.pos.x, this.pos.y, nFrameWidth, nFrameHeight);
+
+        this.headPanel.onDraw(context, nFrameWidth, nFrameHeight);
     }
 }
---
+++ Peace/navigation.js
@@ -0,0 +1,30 @@
+import { Point } from './point.js';
+import { NavItem } from './navItem.js';
+
+const START_X_POS       = 10;
+const START_Y_POS       = 10;
+// const NAV_WIDTH      = 80;
+// const NAV_HEIGHT     = 30;
+const LINE_WIDTH        = 2;
+const NAV_LINE_COLOR    = 'rgb(0, 0, 0)';
+
+const ITEM_TABLE_X_POS  = 20;
+const ITEM_TABLE_Y_POS  = 20;
+
+export class Navigation {
+    constructor() {
+        this.pos = new Point(START_X_POS, START_Y_POS);
+        this.items = [];
+
+        this.items.push(new NavItem('Table', ITEM_TABLE_X_POS, ITEM_TABLE_Y_POS));    
+    }
+
+    onDraw(context, nHeadPanelWidth, nHeadPanelHeight) {
+        context.strokeStyle = NAV_LINE_COLOR;
+        context.lineWidth = LINE_WIDTH;
+        
+        for(let i = 0; i < this.items.length; i++) {
+            this.items[i].onDraw(context, i, nHeadPanelWidth, nHeadPanelHeight);
+        }
+    }    
+} 
