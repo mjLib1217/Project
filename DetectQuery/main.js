50978a813803488a7f743936a28d867b3ed00646
mjLib1217 <mzecatjrotus1217@gmail.com>
2021-01-16 23:33:26 +0900

    Create Project

---
+++ DetectQuery/main.js
@@ -0,0 +1,23 @@
+const colors = require('colors');
+const fs = require('fs');
+const jParser = requre('jparser');
+
+console.log('RUN START'.yellow);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log('RUN END'.yellow);
